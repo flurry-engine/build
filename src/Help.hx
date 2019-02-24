92ddd3a046e7c1ab602f9d9be2c76e97b3511193
Aidan <aidan.lee63@gmail.com>
2019-02-24 16:12:43 +0000

    Initial build tool push

---
+++ src/Help.hx
@@ -0,0 +1,56 @@
+package;
+
+import hxp.Script;
+import hxp.Log;
+
+/**
+ * Script which prints help info for the build command options.
+ */
+class Help extends Script
+{
+    public function new()
+    {
+        super();
+
+        switch (command)
+        {
+            case 'build':
+                printBuildHelp();
+
+            case 'run':
+                printRunHelp();
+
+            case 'package':
+                printPackageHelp();
+
+            case _:
+                printHelp();
+        }
+    }
+
+    function printBuildHelp()
+    {
+        //
+    }
+
+    function printRunHelp()
+    {
+        //
+    }
+
+    function printPackageHelp()
+    {
+        //
+    }
+
+    function printHelp()
+    {
+        Log.println('Flurry 0.0.1');
+        Log.println('');
+        Log.println('Commands');
+        Log.println('    build   - Build a flurry project.');
+        Log.println('    run     - Build and run a flurry project.');
+        Log.println('    package - Build and package a flurry project for distribution');
+        Log.println('    help    - Display detailed information on a command and all its options.');
+    }
+}
---
+++ src/Main.hx
@@ -0,0 +1,150 @@
+package;
+
+import sys.FileSystem;
+import hxp.Haxelib;
+import hxp.Path;
+import hxp.System;
+import hxp.Log;
+
+class Main
+{
+	static function main()
+	{
+		new Main();
+	}
+
+	/**
+	 * The command line arguments passed when this program was ran.
+	 * The first argument will be cut off to identify which sub script to run.
+	 */
+	final arguments : Array<String>;
+
+	public function new()
+	{
+		arguments = Sys.args();
+
+		// When the tool is ran from haxelib the CWD is the root directory of the haxelib.
+		// Haxelib also appends the CWD where it was called from as a last argument and sets the 'HAXELIB_RUN' env.
+		// So if we are running in haxelib mode set the CWD to the last cli argument.
+		if (Sys.getEnv('HAXELIB_RUN') == '1')
+		{
+			var cwd = arguments.pop();
+			if (cwd != null)
+			{
+				Sys.setCwd(cwd);
+			}
+		}
+
+		switch (arguments.shift())
+		{
+
+			case 'build':
+				doBuildCommand('build');
+
+			case 'run':
+				doBuildCommand('run');
+
+			case 'package':
+				doBuildCommand('package');
+
+			case 'clean':
+				doBuildCommand('clean');
+
+			case _:
+				doHelpCommand();
+		}
+	}
+
+	/**
+	 * Run the build script with a command.
+	 * @param _command The command for the build script to decide what to do.
+	 */
+	function doBuildCommand(_command : String)
+	{
+		var script  = findBuildScript();
+		if (script != '')
+		{
+			Log.println(script);
+
+			runHxpScript(script, _command);
+		}
+		else
+		{
+			Log.println('Cound not find a suitable script file to build');
+		}
+	}
+
+	/**
+	 * Prints help about the entire tool or a specific command.
+	 */
+	function doHelpCommand()
+	{
+		var script  = Path.join([ Haxelib.getPath(new Haxelib('build')), 'src', 'Help.hx' ]);
+		var command = arguments.length == 0 ? 'default' : arguments.shift();
+
+		if (command != null)
+		{
+			runHxpScript(script, command);
+		}
+	}
+
+	/**
+	 * Run a .hxp script with the correct flurry classes imported.
+	 * @param _script Script file to run.
+	 * @param _command Optional command for the script.
+	 */
+	function runHxpScript(_script : String, _command : String)
+	{
+		Log.verbose = true;
+		
+		Log.info("", Log.accentColor + "Executing script: " + _script + Log.resetColor);
+		
+		var dir = Path.directory (_script);
+		var file = Path.withoutDirectory (_script);
+		var className = Path.withoutExtension (file);
+		className = className.substr (0, 1).toUpperCase () + className.substr (1);
+		
+		var buildArgs = [ "-cp", FileSystem.absolutePath(Path.combine (Haxelib.getPath (new Haxelib ("hxp")), "src")), "-cp", FileSystem.absolutePath(Path.combine (Haxelib.getPath (new Haxelib ("build")), "src")) ];
+		var runArgs = [ (_command == null || _command == "") ? "default" : _command ];
+		runArgs = runArgs.concat (arguments);
+		
+		runArgs.push (className);
+		runArgs.push (Sys.getCwd ());
+		
+		System.runScript (_script, buildArgs, runArgs, dir);
+	}
+
+	/**
+	 * will look for a build script in the following order.
+	 * - If there is a file which matches the first argument, use that.
+	 * - If there is a file called build.hxp, use that.
+	 * - If there is a file called build.hx, use that.
+	 * - If none of the above are found, error out.
+	 * @return Script file name or empty string.
+	 */
+	function findBuildScript() : String
+	{
+		if (arguments.length > 0 && FileSystem.exists(arguments[0]))
+		{
+			var script = arguments.shift();
+			if (script != null)
+			{
+				return FileSystem.fullPath(script);
+			}
+			
+			return '';
+		}
+
+		if (FileSystem.exists('build.hxp'))
+		{
+			return FileSystem.fullPath('build.hxp');
+		}
+
+		if (FileSystem.exists('build.hx'))
+		{
+			return FileSystem.fullPath('build.hx');
+		}
+
+		return '';
+	}
+}
